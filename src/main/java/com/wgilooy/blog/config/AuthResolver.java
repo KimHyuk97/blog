b384b3c8df6604df23e6766733ceb489c35b1190
LeeJiHyeon <xnfxnf97@naver.com>
2023-02-15 22:26:02 +0900

    response cookie~

--- src/main/java/com/wgilooy/blog/config/AuthResolver.java
+++ src/main/java/com/wgilooy/blog/config/AuthResolver.java
@@ -1,5 +1,8 @@
 package com.wgilooy.blog.config;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -13,7 +16,9 @@ import com.wgilooy.blog.exception.UnAuthorized;
 import com.wgilooy.blog.repositroy.TokenRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -29,17 +34,29 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer,
             NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) throws Exception {
 
-        String accessToken = webRequest.getHeader("Authorization");
+        // String accessToken = webRequest.getHeader("Authorization");
 
-        if(accessToken == null || accessToken.equals("")) {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (request == null) {
+            log.error("사용자 인증 request null");
+            throw new UnAuthorized();
+        }
+        Cookie[] cookies = request.getCookies();
+        if (cookies.length == 0) {
+            log.error("사용자 인증 cookie null");
             throw new UnAuthorized();
         }
 
-        Token token = tokenRepository.findByAccessToken(accessToken)
-            .orElseThrow(UnAuthorized:: new);
+        // if(accessToken == null || accessToken.equals("")) {
+        // throw new UnAuthorized();
+        // }
 
+        String accessToken = cookies[0].getValue();
+
+        Token token = tokenRepository.findByAccessToken(accessToken)
+                .orElseThrow(UnAuthorized::new);
 
         return new UserSession(token.getUser().getId());
     }
-    
+
 }
--- src/main/java/com/wgilooy/blog/controller/AuthController.java
+++ src/main/java/com/wgilooy/blog/controller/AuthController.java
@@ -1,13 +1,17 @@
 package com.wgilooy.blog.controller;
 
+import java.time.Duration;
+
 import javax.validation.Valid;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wgilooy.blog.dto.Login;
-import com.wgilooy.blog.response.TokenResponse;
 import com.wgilooy.blog.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,12 +21,25 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final AuthService authService;
-    
-    @PostMapping("/auth/login")
-    public TokenResponse login(@RequestBody @Valid Login request) {
-        String accessToken = authService.signin(request);
 
-        return new TokenResponse(accessToken);
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@RequestBody @Valid Login request) {
+        String accessToken = authService.signin(request);
+        ResponseCookie cookie = ResponseCookie
+                .from("SESSION", accessToken)
+                .domain("localhost")
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        System.out.println(cookie);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 
 }
